720166da3a08d3670a61b6701f620c9e7ddad588
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-12-01 14:01:12 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,115 @@
+{
+  description = "Build a cargo project with a custom toolchain";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-parts,
+    rust-overlay,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.git-hooks-nix.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      flake = {};
+      perSystem = {
+        pkgs,
+        system,
+        config,
+        ...
+      }: let
+        # NB: we don't need to overlay our custom toolchain for the *entire*
+        # pkgs (which would require rebuidling anything else which uses rust).
+        # Instead, we just want to update the scope that crane will use by appending
+        # our specific toolchain there.
+        craneLib = (crane.mkLib pkgs).overrideToolchain (p:
+          p.rust-bin.stable.latest.default.override {
+            extensions = ["rust-src"];
+          });
+
+        my-crate = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+          doCheck = true;
+
+          buildInputs =
+            [
+              # Add additional build inputs here
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
+        };
+      in {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
+
+        checks = {
+          inherit my-crate;
+        };
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = let
+            rust = pkgs.rust-bin.stable.latest;
+          in {
+            alejandra.enable = true;
+            rustfmt = {
+              enable = true;
+              packageOverrides = {
+                inherit (rust) rustfmt cargo;
+              };
+            };
+          };
+        };
+
+        packages.default = my-crate;
+
+        devShells.default = craneLib.devShell {
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Extra inputs can be added here; cargo and rustc are provided by default
+          # from the toolchain that was specified earlier.
+          packages = [
+          ];
+        };
+      };
+    };
+}
