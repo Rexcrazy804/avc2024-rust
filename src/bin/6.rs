62b27314b289b4f416657f7106efd0db3076cad2
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-12-06 22:30:39 +0530

    day 6 part 2 complete :)

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -35,11 +35,12 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    //println!("\n=== Part 2 ===");
-    //assert_eq!(123, part2(BufReader::new(TEST.as_bytes()))?);
-    //let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    //let result = time_snippet!(part2(input_file)?);
-    //println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+    assert_eq!(6, part2(BufReader::new(TEST.as_bytes()))?);
+    println!("TEST PASSED");
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
@@ -96,72 +97,141 @@ fn part1<R: BufRead>(reader: R) -> Result<usize> {
 
 fn move_guard(
     mut direction: usize,
-    player_pos: (usize, usize),
+    mut player_pos: (usize, usize),
     map: &Vec<Vec<char>>,
     tiles_covered: &mut HashSet<(usize, usize)>,
 ) {
-    tiles_covered.insert(player_pos);
-
     let max_rows = map.len();
     let max_cols = map[0].len();
 
-    //dbg!(direction);
-    //dbg!(player_pos);
-
-    // 1. insert current position
-    // 1.5 check if next position is valid, if not EXIT RECURSION
-    // 2. check if next position is an obstacles
-    // 3. if 2. is yes {
-    //  incremenet direction
-    //  goto 1.5
-    // }
-    // 4. RECURSION POINT update player_pos to next_pos and call the function again (1.)
-
     loop {
-        let next_position = match &DIRECTIONS[direction] {
-            Direction::Up => {
-                if player_pos.0 < 1 {
-                    return;
-                }
-                (player_pos.0 - 1, player_pos.1)
-            }
-            Direction::Right => {
-                if player_pos.1 == max_cols - 1 {
-                    return;
-                }
-                (player_pos.0, player_pos.1 + 1)
-            }
-            Direction::Down => {
-                if player_pos.0 == max_rows - 1 {
-                    return;
-                }
-                (player_pos.0 + 1, player_pos.1)
-            }
-            Direction::Left => {
-                if player_pos.1 < 1 {
-                    return;
-                }
-                (player_pos.0, player_pos.1 - 1)
-            }
+        tiles_covered.insert(player_pos);
+        let next_position = match get_next_position(direction, player_pos, max_cols, max_rows) {
+            Some(value) => value,
+            None => return,
         };
 
-        if is_barricade(next_position, map).is_none() {
+        if is_barricade(next_position, map) {
             direction = if direction == 3 { 0 } else { direction + 1 };
         } else {
-            move_guard(direction, next_position, map, tiles_covered);
-            break;
+            player_pos = next_position;
         }
     }
 }
 
-fn is_barricade(next_position: (usize, usize), map: &Vec<Vec<char>>) -> Option<()> {
-    if *map.get(next_position.0)?.get(next_position.1)? != '#' {
-        Some(())
-    } else {
-        None
-    }
+fn get_next_position(
+    direction: usize,
+    player_pos: (usize, usize),
+    max_cols: usize,
+    max_rows: usize,
+) -> Option<(usize, usize)> {
+    let next_position = match &DIRECTIONS[direction] {
+        Direction::Up => {
+            if player_pos.0 < 1 {
+                return None;
+            }
+            (player_pos.0 - 1, player_pos.1)
+        }
+        Direction::Right => {
+            if player_pos.1 == max_cols - 1 {
+                return None;
+            }
+            (player_pos.0, player_pos.1 + 1)
+        }
+        Direction::Down => {
+            if player_pos.0 == max_rows - 1 {
+                return None;
+            }
+            (player_pos.0 + 1, player_pos.1)
+        }
+        Direction::Left => {
+            if player_pos.1 < 1 {
+                return None;
+            }
+            (player_pos.0, player_pos.1 - 1)
+        }
+    };
+    Some(next_position)
+}
+
+fn is_barricade(next_position: (usize, usize), map: &Vec<Vec<char>>) -> bool {
+    *map.get(next_position.0)
+        .expect("Invalid row")
+        .get(next_position.1)
+        .expect("Invalid column")
+        == '#'
 }
 
 fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    todo!()
+    let mut map: Vec<Vec<char>> = Vec::new();
+    let mut player_start_pos: Option<(usize, usize)> = None;
+
+    for (row_index, line) in reader.lines().enumerate() {
+        let tiles = line?.chars().collect::<Vec<char>>();
+
+        if player_start_pos.is_none() {
+            //dbg!(&tiles);
+            // could potentially optimize this if I can find an std function
+            // that can search and return the index of the PLAYER_CHARACTER
+            // I am looking for
+            for (col_index, tile) in tiles.iter().enumerate() {
+                if *tile == PLAYER_CHARACTER {
+                    player_start_pos = Some((row_index, col_index));
+                }
+            }
+        }
+
+        map.push(tiles);
+    }
+
+    let initial_direction = 0;
+    let Some(player_pos) = player_start_pos else {
+        panic!("Player not found")
+    };
+    let mut tiles_covered: HashSet<(usize, usize)> = HashSet::new();
+
+    move_guard(initial_direction, player_pos, &map, &mut tiles_covered);
+
+    let answer = tiles_covered
+        .iter()
+        .skip(1)
+        .filter(|x| does_guard_loop(player_pos, **x, &map))
+        .count();
+
+    Ok(answer)
+}
+
+fn does_guard_loop(
+    mut player_pos: (usize, usize),
+    obstacle: (usize, usize),
+    map: &Vec<Vec<char>>,
+) -> bool {
+    let max_rows = map.len();
+    let max_cols = map[0].len();
+    let mut tiles_covered: HashSet<((usize, usize), usize)> = HashSet::new();
+    let mut direction = 0;
+    let mut next_direction = 0;
+
+    loop {
+        let next_position = match get_next_position(next_direction, player_pos, max_cols, max_rows)
+        {
+            Some(value) => value,
+            None => return false,
+        };
+
+        if is_barricade(next_position, map) || obstacle == next_position {
+            next_direction = if next_direction == 3 {
+                0
+            } else {
+                next_direction + 1
+            };
+        } else {
+            if !tiles_covered.insert((player_pos, direction)) {
+                return true;
+            }
+
+            player_pos = next_position;
+            direction = next_direction;
+        }
+    }
 }
